0a94c2d4a4aeed8389e8505a263e70f9449fd848
Nikitatopodin <Nikitatop1@gmail.com>
2023-04-26 18:26:17 +0300

    feat: change array of keys to object, add basic shift conditions

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ class Keyboard {
     textArea: null,
     wrapper: null,
     board: null,
-    keys: [],
     capsLock: false,
     value: '',
-    shiftLeft: false,
-    shiftRight: false,
+    shift: false,
   };
 
   eventHanlders = {
@@ -33,42 +31,102 @@ class Keyboard {
   }
 
   createKeys() {
-    const keys = [
-      '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Backspace',
-      'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
-      'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter',
-      'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'up', 'Shift',
-      'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', 'left', 'down', 'right', 'DEL',
-    ];
-
+    const keysObj = {
+      Backquote: '`',
+      Digit1: '!',
+      Digit2: '2',
+      Digit3: '3',
+      Digit4: '4',
+      Digit5: '5',
+      Digit6: '6',
+      Digit7: '7',
+      Digit8: '8',
+      Digit9: '9',
+      Digit0: '0',
+      Minus: '-',
+      Equal: '=',
+      Backspace: 'Backspace',
+      Tab: 'Tab',
+      KeyQ: 'q',
+      KeyW: 'w',
+      KeyE: 'e',
+      KeyR: 'r',
+      KeyT: 't',
+      KeyY: 'y',
+      KeyU: 'u',
+      KeyI: 'i',
+      KeyO: 'o',
+      KeyP: 'p',
+      BracketLeft: '[',
+      BracketRight: ']',
+      Backslash: '\\',
+      CapsLock: 'CapsLock',
+      KeyA: 'a',
+      KeyS: 's',
+      KeyD: 'd',
+      KeyF: 'f',
+      KeyG: 'g',
+      KeyH: 'h',
+      KeyJ: 'j',
+      KeyK: 'k',
+      KeyL: 'l',
+      Semicolon: ';',
+      Quote: "'",
+      Enter: 'Enter',
+      ShiftLeft: 'Shift',
+      KeyZ: 'z',
+      KeyX: 'x',
+      KeyC: 'c',
+      KeyV: 'v',
+      KeyB: 'b',
+      KeyN: 'n',
+      KeyM: 'm',
+      Comma: ',',
+      Period: '.',
+      Slash: '/',
+      ArrowUp: 'up',
+      ShiftRight: 'Shift',
+      ControlLeft: 'Ctrl',
+      MetaLeft: 'Win',
+      AltLeft: 'Alt',
+      Space: 'Space',
+      AltRight: 'Alt',
+      ControlRight: 'Ctrl',
+      ArrowLeft: 'left',
+      ArrowDown: 'down',
+      ArrowRight: 'right',
+      Delete: 'DEL',
+    };
+    const keys = Object.entries(keysObj);
     const edgeKeys = ['Backspace', '\\', 'Enter', 'Shift'];
     const wideKeys = ['CapsLock', 'Enter', 'Shift', 'Backspace', 'Tab'];
 
-    keys.forEach((key, index) => {
+    keys.forEach((pair, index) => {
       const keyBtn = document.createElement('button');
       const br = document.createElement('br');
-
-      keyBtn.textContent = key;
+      const keyValue = pair[1];
+      const keyCode = pair[0];
+      keyBtn.textContent = keyValue;
       keyBtn.classList.add('keyboard__key');
       this.props.board.append(keyBtn);
 
       // Adding break lines for correct keyboard display
-      if (edgeKeys.includes(key)) {
-        if (key === 'Shift' && keys[index - 1] === 'up') {
+      if (edgeKeys.includes(keyValue)) {
+        if (keyValue === 'Shift' && keys[index - 1] === 'up') {
           this.props.board.append(br);
         }
 
-        if (key !== 'Shift') {
+        if (keyValue !== 'Shift') {
           this.props.board.append(br);
         }
       }
 
-      if (wideKeys.includes(key)) {
+      if (wideKeys.includes(keyValue)) {
         keyBtn.classList.add('keyboard__key_wide');
       }
 
       // Special cases
-      if (key === 'Space') {
+      if (keyValue === 'Space') {
         keyBtn.classList.add('keyboard__key_space', 'Space');
 
         keyBtn.addEventListener('mousedown', () => {
@@ -83,8 +141,8 @@ class Keyboard {
 
         keyBtn.addEventListener('keydown', (e) => this.downKey(e));
         keyBtn.addEventListener('keyup', (e) => this.upKey(e));
-      } else if (key === 'CapsLock') {
-        keyBtn.classList.add(key);
+      } else if (keyValue === 'CapsLock') {
+        keyBtn.classList.add(keyValue);
 
         keyBtn.addEventListener('mousedown', () => {
           this.toggleCapsLock();
@@ -93,13 +151,19 @@ class Keyboard {
 
         keyBtn.addEventListener('keydown', (e) => this.downKey(e));
         keyBtn.addEventListener('keyup', (e) => this.upKey(e));
-      } else if (key === 'Backspace') {
+      } else if (keyValue === 'Backspace') {
+        keyBtn.classList.add(keyValue);
+
         keyBtn.addEventListener('mousedown', () => {
+          keyBtn.classList.add('keyboard__key_active');
           this.props.value = this.props.value.substring(0, this.props.value.length - 1);
           this.props.textArea.value = this.props.value;
         });
-      } else if (key === 'Enter') {
-        keyBtn.classList.add(key);
+        keyBtn.addEventListener('mouseup', () => {
+          keyBtn.classList.remove('keyboard__key_active');
+        });
+      } else if (keyValue === 'Enter') {
+        keyBtn.classList.add(keyValue);
 
         keyBtn.addEventListener('mousedown', () => {
           keyBtn.classList.add('keyboard__key_active');
@@ -113,8 +177,8 @@ class Keyboard {
 
         keyBtn.addEventListener('keydown', (e) => this.downKey(e));
         keyBtn.addEventListener('keyup', (e) => this.upKey(e));
-      } else if (key === 'Tab') {
-        keyBtn.classList.add(key);
+      } else if (keyValue === 'Tab') {
+        keyBtn.classList.add(keyValue);
 
         keyBtn.addEventListener('mousedown', () => {
           keyBtn.classList.add('keyboard__key_active');
@@ -128,11 +192,39 @@ class Keyboard {
 
         keyBtn.addEventListener('keydown', (e) => this.downKey(e));
         keyBtn.addEventListener('keyup', (e) => this.upKey(e));
-      } else {
-        keyBtn.classList.add(`Key${key.toUpperCase()}`);
+      } else if (keyValue === 'Shift') {
+        keyBtn.classList.add(keyCode);
 
         keyBtn.addEventListener('mousedown', () => {
-          this.props.value += this.props.capsLock ? key.toUpperCase() : key.toLowerCase();
+          keyBtn.classList.add('keyboard__key_active');
+          this.props.shift = true;
+          this.toggleShift();
+        });
+
+        keyBtn.addEventListener('mouseup', () => {
+          keyBtn.classList.remove('keyboard__key_active');
+          this.props.shift = false;
+          this.toggleShift();
+        });
+
+        keyBtn.addEventListener('keydown', (e) => this.downKey(e));
+        keyBtn.addEventListener('keyup', (e) => this.upKey(e));
+      } else {
+        keyBtn.classList.add(keyCode);
+
+        keyBtn.addEventListener('mousedown', () => {
+          const keyBtns = document.querySelectorAll('.keyboard__key');
+          keyBtns.forEach((k) => {
+            if (this.props.capsLock || this.props.shift) {
+              if (k.classList.contains(keyCode)) {
+                this.props.value += k.textContent.toUpperCase();
+              }
+            } else if (!this.props.capsLock && !this.props.shift) {
+              if (k.classList.contains(keyCode)) {
+                this.props.value += k.textContent.toLowerCase();
+              }
+            }
+          });
           keyBtn.classList.add('keyboard__key_active');
           this.props.textArea.value = this.props.value;
         });
@@ -141,7 +233,7 @@ class Keyboard {
           const keyBtns = document.querySelectorAll('.keyboard__key');
 
           keyBtns.forEach((keyElem) => {
-            if (keyElem.classList.contains(`Key${keyElem.textContent[0].toUpperCase()}`)) {
+            if (keyElem.classList.contains(keyCode)) {
               keyElem.classList.remove('keyboard__key_active');
             }
           });
@@ -155,6 +247,30 @@ class Keyboard {
 
   toggleCapsLock() {
     this.props.capsLock = !this.props.capsLock;
+  }
+
+  toggleShift() {
+    const nonShiftSpecialKeys = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '[', ']', '\\', ';', "'", ',', '.', '/'];
+    const shiftSpecialKeys = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '{', '}', '|', ':', '"', '<', '>', '?'];
+    const keyBtns = document.querySelectorAll('.keyboard__key');
+
+    keyBtns.forEach((k) => {
+      if (this.props.shift) {
+        if (k.textContent.length === 1) {
+          k.textContent = k.textContent.toUpperCase();
+          if (nonShiftSpecialKeys.indexOf(k.textContent) !== -1) {
+            k.textContent = shiftSpecialKeys[nonShiftSpecialKeys.indexOf(k.textContent)];
+          }
+        }
+      } else if (!this.props.shift) {
+        if (k.textContent.length === 1) {
+          k.textContent = k.textContent.toLowerCase();
+          if (shiftSpecialKeys.indexOf(k.textContent) !== -1) {
+            k.textContent = nonShiftSpecialKeys[shiftSpecialKeys.indexOf(k.textContent)];
+          }
+        }
+      }
+    });
   }
 
   downKey(e) {
@@ -173,8 +289,15 @@ class Keyboard {
         } else if (keyElem.textContent === 'Tab') {
           this.props.value += '  ';
           keyElem.classList.add('keyboard__key_active');
+        } else if (keyElem.textContent === 'Backspace') {
+          this.props.value = this.props.value.substring(0, this.props.value.length - 1);
+          keyElem.classList.add('keyboard__key_active');
+        } else if (keyElem.textContent === 'Shift') {
+          this.props.shift = true;
+          keyElem.classList.add('keyboard__key_active');
+          this.toggleShift();
         } else {
-          if (this.props.capsLock) {
+          if (this.props.capsLock || this.props.shift) {
             this.props.value += keyElem.textContent.toUpperCase();
           } else {
             this.props.value += keyElem.textContent.toLowerCase();
@@ -192,6 +315,10 @@ class Keyboard {
       if (keyElem.classList.contains(e.code)) {
         if (keyElem.textContent === 'CapsLock') {
           keyElem.classList.toggle('keyboard__key_active', this.props.capsLock);
+        } else if (keyElem.textContent === 'Shift') {
+          this.props.shift = false;
+          keyElem.classList.remove('keyboard__key_active');
+          this.toggleShift();
         } else {
           keyElem.classList.remove('keyboard__key_active');
         }
